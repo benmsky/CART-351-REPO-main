9bd26a255c3b4de22ba87b9697e169181adb268a
Ben <benmacknofsky@gmail.com>
2026-09-23 16:22:21 -0400

    worked with python

---
+++ CART-351-REPO-main/EXERCISES/hello.py
@@ -0,0 +1,14 @@
+# # a comment in python
+# # single line
+# print("this is correct")
+# # print(25+30/7) # float division
+# # print(25+30//7) # integer division
+# print(3+2<5-7)
+# testVar = 5.5
+# #print(type(testVar))
+my_name = "Ben"
+# my_fav_fruit = "raspberry" 
+# saved_str =f"my favorite fruit is {my_fav_fruit}"
+# my_fav_fruit = "banana"
+# print(saved_str)
+my_name=input("name: ")
